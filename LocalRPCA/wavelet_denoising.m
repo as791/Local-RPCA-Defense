539c6db9213011096f11c366c5c19a6cc0e8c38e
Aryaman Sinha <aryamansinha123@gmail.com>
2023-03-03 00:19:40 +0530

    Create wavelet_denoising.m

---
+++ LocalRPCA/wavelet_denoising.m
@@ -0,0 +1,5 @@
+% Copyright (C) 2021, Aryaman Sinha
+function [img] = wavelet_denoising(img,sigma)
+    img = double(py.skimage.restoration.denoise_wavelet(pyargs('image',py.numpy.array(img/255), 'sigma',sigma(1), 'mode','soft', 'multichannel',true,... 
+    'convert2ycbcr',true,'method','BayesShrink','wavelet','db1')))*255;
+end
